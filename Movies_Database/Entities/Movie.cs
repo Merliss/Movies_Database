8dba2493448b0872f069a12aa679613ad6c86076
Merlis <merlishjzkl@gmail.com>
2023-11-09 16:58:14 +0100

    Add project files.

---
+++ Movies_Database/Entities/Movie.cs
@@ -0,0 +1,26 @@
+﻿namespace Movies_Database.Entities
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Year { get; set; }
+
+        public int DirectorId { get; set; }
+        public int CountryId { get; set; }
+        public int GenreId { get; set; }
+
+        public int MovieRatingId { get; set; }
+
+        public virtual Director Director { get; set; }
+
+        public virtual Country Country { get; set; }
+        public virtual Genre Genre { get; set; }
+
+        public virtual MovieRating MovieRating { get; set; }
+
+
+
+    }
+}
---
+++ Movies_Database/Entities/MovieRating.cs
@@ -0,0 +1,6 @@
+﻿namespace Movies_Database.Entities
+{
+    public class MovieRating
+    {
+    }
+}
