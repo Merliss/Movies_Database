e9c16292009faf8c02fb9aa68dd82bf3c9a5bfcc
Merlis <merlishjzkl@gmail.com>
2023-11-10 12:17:36 +0100

    Mapping Profile from AutoMapper plugin  added and registered

---
+++ Movies_Database/MovieMappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Movies_Database.Entities;
+using Movies_Database.Models;
+
+namespace Movies_Database
+{
+    public class MovieMappingProfile : Profile
+    {
+        public MovieMappingProfile()
+        {
+            CreateMap<Movie, MovieDto>()
+                .ForMember(m => m.DirectorName, c => c.MapFrom(s => s.Director.Name))
+                .ForMember(m => m.DirectorSurname, c => c.MapFrom(s => s.Director.Surname))
+                .ForMember(m => m.Country, c => c.MapFrom(s => s.Country.Name))
+                .ForMember(m => m.Genre, c => c.MapFrom(s => s.Genre.Name));
+
+            CreateMap<MovieRating, MovieRatingDto>();
+        }
+
+
+    }
+}
--- Movies_Database/Program.cs
+++ Movies_Database/Program.cs
@@ -1,14 +1,23 @@
+using Movies_Database;
+using Movies_Database.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<MovieDbContext>();
+builder.Services.AddScoped<MovieSeeder>();
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+var scope = app.Services.CreateScope();
+var seeder = scope.ServiceProvider.GetRequiredService<MovieSeeder>();
 
+seeder.Seed();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
