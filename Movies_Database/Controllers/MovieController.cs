2da0eb5c7ed0308d8c3f34b25a5f585425330bee
Merlis <merlishjzkl@gmail.com>
2023-11-10 11:49:10 +0100

    Movie controller first version

---
+++ Movies_Database/Controllers/MovieController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies_Database.Entities;
+using Movies_Database.Models;
+
+namespace Movies_Database.Controllers
+{
+
+    [Route("api/movie")]
+    public class MovieController : Controller
+    {
+        private readonly MovieDbContext _dbContext;
+
+        public MovieController(MovieDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<MovieDto>> GetAll()
+        {
+            var movies = _dbContext
+                .Movies
+                .ToList();
+            return Ok(movies);
+        }
+
+
+        [HttpGet("{id}")]
+        public ActionResult<MovieDto> Get([FromRoute] int id)
+        {
+            var movie = _dbContext
+                .Movies
+                .FirstOrDefault(x => x.Id == id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return movie;
+        }
+    }
+}
