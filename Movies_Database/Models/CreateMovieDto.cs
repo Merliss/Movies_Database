91fb6a8f7beaff0ef46727fffe6b41758c544f48
Merlis <merlishjzkl@gmail.com>
2023-11-17 00:12:28 +0100

    Created dto for movie and mapping profile for mapper.

---
+++ Movies_Database/Models/CreateMovieDto.cs
@@ -0,0 +1,21 @@
+﻿namespace Movies_Database.Models
+{
+    public class CreateMovieDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Year { get; set; }
+
+        public string DirectorName { get; set; }
+
+        public string DirectorSurname { get; set; }
+
+        public string Country { get; set; }
+
+        public string Genre { get; set; }
+
+
+
+    }
+}
--- Movies_Database/MovieMappingProfile.cs
+++ Movies_Database/MovieMappingProfile.cs
@@ -15,7 +15,15 @@ namespace Movies_Database
                 .ForMember(m => m.Genre, c => c.MapFrom(s => s.Genre.Name));
 
             CreateMap<MovieRating, MovieRatingDto>();
-        }
+
+
+            CreateMap<CreateMovieDto, Movie>()
+                .ForMember(m => m.Director, c => c.MapFrom(dto => new Director() { Name = dto.DirectorName, Surname = dto.DirectorSurname }))
+                .ForMember(m => m.Genre, c => c.MapFrom(dto => new Genre() { Name = dto.Genre }))
+                .ForMember(m => m.Country, c => c.MapFrom(dto => new Country() { Name = dto.Country}));
+
+        }       
+
 
 
     }
