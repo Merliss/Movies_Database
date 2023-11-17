d09e0293a1815936288dfdf3a98877e29dfe294a
Merlis <merlishjzkl@gmail.com>
2023-11-17 17:57:19 +0100

    Controller refactor, service for movies added

--- Movies_Database/Controllers/MovieController.cs
+++ Movies_Database/Controllers/MovieController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Movies_Database.Entities;
 using Movies_Database.Models;
+using Movies_Database.Services;
 
 namespace Movies_Database.Controllers
 {
@@ -10,27 +11,18 @@ namespace Movies_Database.Controllers
     [Route("api/movie")]
     public class MovieController : Controller
     {
-        private readonly MovieDbContext _dbContext;
-        private readonly IMapper _mapper;
+        private readonly IMovieService _movieService;
+        
 
-        public MovieController(MovieDbContext dbContext, IMapper mapper)
+        public MovieController(IMovieService movieService)
         {
-            _dbContext = dbContext;
-            _mapper = mapper;
+            _movieService = movieService;
         }
 
         [HttpGet]
         public ActionResult<IEnumerable<MovieDto>> GetAll()
         {
-            var movies = _dbContext
-                .Movies
-                .Include(m => m.Director)
-                .Include(m => m.Country)
-                .Include(m => m.Genre)
-                .Include(m => m.MovieRatings)
-                .ToList();
-            
-            var moviesDtos = _mapper.Map<List<MovieDto>>(movies);
+            var moviesDtos = _movieService.GetAllMovies();
 
             return Ok(moviesDtos);
         }
@@ -39,69 +31,30 @@ namespace Movies_Database.Controllers
         [HttpGet("{id}")]
         public ActionResult<MovieDto> Get([FromRoute] int id)
         {
-            var movie = _dbContext
-                .Movies
-                .Include(m => m.Director)
-                .Include(m => m.Country)
-                .Include(m => m.Genre)
-                .Include(m => m.MovieRatings)
-                .FirstOrDefault(x => x.Id == id);
+            var movieDto = _movieService.GetMovieById(id);
 
-            if (movie == null)
+            if (movieDto == null)
             {
                 return NotFound();
             }
 
-            var movieDto = _mapper.Map<MovieDto>(movie);
+            
             return Ok(movieDto);
         }
 
         [HttpPost]
         public ActionResult CreateMovie([FromBody] CreateMovieDto dto)
         {
-            if (!ModelState.IsValid)
+            var id = _movieService.Create(dto);
+           
+            if (id == -1)
             {
-                return BadRequest(ModelState);
+                return BadRequest(dto);
             }
 
-            var movie = _mapper.Map<Movie>(dto);
-            var genreName = movie.Genre.Name;
-            var countryName = movie.Country.Name;
-            var directorSurname = movie.Director.Surname;
-            var directorName = movie.Director.Name;
-            var existingGenre = _dbContext.Genres.FirstOrDefault(g => g.Name == genreName);
-            var existingCountry = _dbContext.Countries.FirstOrDefault(g => g.Name == countryName);
-            var existingDirector = _dbContext.Directors.FirstOrDefault(g => g.Name == directorName && g.Surname == directorSurname);
+
             
-            var existingMovie = _dbContext.Movies.FirstOrDefault(g => g.Name == movie.Name);
-
-            if (existingMovie != null)
-            {
-                return BadRequest("Movie exist");
-            }
-
-            if (existingGenre != null)
-            {
-                movie.Genre = existingGenre;
-            }
-
-            if (existingCountry != null)
-            {
-                movie.Country = existingCountry;
-            }
-
-            if (existingDirector != null)
-            {
-                movie.Director = existingDirector;
-            }
-
-
-
-            _dbContext.Movies.Add(movie);
-
-            _dbContext.SaveChanges();
-
-            return Created($"/api/movie/{movie.Id}", null);
+            return Created($"/api/movie/{id}", null);
 
         }
 
---
+++ Movies_Database/Services/MovieService.cs
@@ -0,0 +1,104 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Movies_Database.Entities;
+using Movies_Database.Models;
+
+namespace Movies_Database.Services
+{
+
+    public interface IMovieService
+    {
+        MovieDto GetMovieById(int id);
+        IEnumerable<MovieDto> GetAllMovies();
+        int Create(CreateMovieDto dto);
+    }
+    public class MovieService : IMovieService
+    {
+        private readonly MovieDbContext _dbContext;
+        private readonly IMapper _mapper;
+        public MovieService(MovieDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public MovieDto GetMovieById (int id)
+        {
+            var movie = _dbContext
+                .Movies
+                .Include(m => m.Director)
+                .Include(m => m.Country)
+                .Include(m => m.Genre)
+                .Include(m => m.MovieRatings)
+                .FirstOrDefault(x => x.Id == id);
+
+            if(movie == null)
+            {
+                return null;
+            }
+
+            var result = _mapper.Map<MovieDto>(movie);
+
+            return result;
+        }
+
+        public IEnumerable<MovieDto> GetAllMovies()
+        {
+            var movies = _dbContext
+                .Movies
+                .Include(m => m.Director)
+                .Include(m => m.Country)
+                .Include(m => m.Genre)
+                .Include(m => m.MovieRatings)
+                .ToList();
+
+            
+
+            var moviesDtos = _mapper.Map<List<MovieDto>>(movies);
+
+            return moviesDtos;
+        }
+
+        public int Create(CreateMovieDto dto)
+        {
+            var movie = _mapper.Map<Movie>(dto);
+            var genreName = movie.Genre.Name;
+            var countryName = movie.Country.Name;
+            var directorSurname = movie.Director.Surname;
+            var directorName = movie.Director.Name;
+            var existingGenre = _dbContext.Genres.FirstOrDefault(g => g.Name == genreName);
+            var existingCountry = _dbContext.Countries.FirstOrDefault(g => g.Name == countryName);
+            var existingDirector = _dbContext.Directors.FirstOrDefault(g => g.Name == directorName && g.Surname == directorSurname);
+
+            var existingMovie = _dbContext.Movies.FirstOrDefault(g => g.Name == movie.Name);
+
+
+            if (existingGenre != null)
+            {
+                movie.Genre = existingGenre;
+            }
+
+            if (existingCountry != null)
+            {
+                movie.Country = existingCountry;
+            }
+
+            if (existingDirector != null)
+            {
+                movie.Director = existingDirector;
+            }
+
+            if (existingMovie != null)
+            {
+                _dbContext.Movies.Add(movie);
+
+                _dbContext.SaveChanges();
+
+                return movie.Id;
+            }
+
+            return -1;
+        }
+
+    }
+}
