b7925fd22e68569c3fd18651b08ddcfd09f02682
Merlis <merlishjzkl@gmail.com>
2023-11-20 22:16:12 +0100

    Validation for registering account added

---
+++ Movies_Database/Models/Validators/RegisterUserDtoValidator.cs
@@ -0,0 +1,48 @@
+﻿using FluentValidation;
+using Movies_Database.Entities;
+
+namespace Movies_Database.Models.Validators
+{
+    public class RegisterUserDtoValidator : AbstractValidator<RegisterUserDto>
+    {
+        public RegisterUserDtoValidator(MovieDbContext dbContext)
+        {
+            RuleFor(x => x.Username).NotEmpty();
+
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .MinimumLength(8);
+
+            RuleFor(x => x.ConfirmPassword)
+                .Equal(p => p.Password);
+            
+
+
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress();
+
+            RuleFor(x => x.Email)
+                .Custom((value, context) =>
+                {
+                    var emailExist = dbContext.Users.Any(x => x.Email == value);
+                    if (emailExist)
+                    {
+                        context.AddFailure("Email already exist");
+                    }
+                });
+
+            RuleFor(x => x.Username)
+                .Custom((value, context) =>
+                {
+                    var usernameExist = dbContext.Users.Any(x => x.Username == value);
+                    if (usernameExist)
+                    {
+                        context.AddFailure("Username already exist");
+                    }
+                });
+        }
+
+    }
+}
