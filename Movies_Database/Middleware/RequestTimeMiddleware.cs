07972d9ef3919cff72f23d6eefddee1d454d1f37
Merlis <merlishjzkl@gmail.com>
2023-11-20 20:16:23 +0100

    RequestTime Middleware Created

---
+++ Movies_Database/Middleware/RequestTimeMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+namespace Movies_Database.Middleware
+{
+    public class RequestTimeMiddleware : IMiddleware
+    {
+        private readonly Stopwatch _stopwatch;
+        private readonly int _timeout = 4;
+        private readonly ILogger<RequestTimeMiddleware> _logger;
+        public RequestTimeMiddleware(ILogger<RequestTimeMiddleware> logger)
+        {
+            _stopwatch = new Stopwatch();
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+
+            _stopwatch.Start();
+            await next.Invoke(context);
+            _stopwatch.Stop();
+
+            var elapsedTime = _stopwatch.ElapsedMilliseconds;
+
+            if(elapsedTime/1000 > _timeout)
+            {
+                var message = $"Request [{context.Request.Method}] at {context.Request.Path} took {elapsedTime} ms";
+
+                _logger.LogInformation(message);
+            }
+        }
+    }
+}
