0641034d42518d1769540b0ba228e1573a194ad4
Merlis <merlishjzkl@gmail.com>
2023-11-24 17:03:36 +0100

    First part of authorization for deleting and updating movie ratings (1/2).

---
+++ Movies_Database/Authorization/ResourceOperationsRequirement.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Movies_Database.Authorization
+{
+
+
+    public enum ResourceOperation
+    {
+        Create,
+        Read,
+        Update,
+        Delete
+    }
+    public class ResourceOperationsRequirement : IAuthorizationRequirement
+    {
+        public ResourceOperationsRequirement(ResourceOperation resourceOperation)
+        {
+            ResourceOperation = resourceOperation;
+        }
+
+        public ResourceOperation ResourceOperation { get; set; }
+
+    }
+}
---
+++ Movies_Database/Authorization/ResourceOperationsRequirementHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Movies_Database.Entities;
+
+namespace Movies_Database.Authorization
+{
+    public class ResourceOperationsRequirementHandler : AuthorizationHandler<ResourceOperationsRequirement,MovieRating>
+    {
+
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ResourceOperationsRequirement requirement, MovieRating movieRating)
+        {
+           if (requirement.ResourceOperation == ResourceOperation.Read ||
+                requirement.ResourceOperation == ResourceOperation.Create)
+            {
+                context.Succeed(requirement);
+            }
+
+           var userId = context.User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier).Value;
+            if (movieRating.UserId == int.Parse(userId))
+            {
+                context.Succeed(requirement);
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
